62d9704fed08177cdd1e197a0533fa90d7962432
shoccho <sfb.shoccho@gmail.com>
2020-09-24 23:39:28 +0600

    added template

---
+++ template/main.cpp
@@ -0,0 +1,36 @@
+#include<windows.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+#include<math.h>
+
+void init(){
+
+    glClearColor(0.0f,0.0f,0.0f,0.0f);
+    glOrtho(-10,10,-10,10,-10,10);
+}
+static void display(void){
+
+    glColor3f(1.0f,0.0f,0.0f);
+    glBegin(GL_QUADS);
+        glVertex2d(5,2);
+        glVertex2d(5,-5);
+        glVertex2d(-5,-5);
+        glVertex2d(-5,2);
+    glEnd();
+    glFlush();
+}
+
+int main(){
+
+    glutInitWindowSize(700,700);
+    glutInitWindowPosition(100,100);
+    glutInitDisplayMode( GLUT_SINGLE|  GLUT_RGB);
+    glutCreateWindow("Template");
+
+    init();
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+
+    return EXIT_SUCCESS;
+}
