f703c69694ab173cf8733aa023f49f8723ac5311
shoccho <sfb.shoccho@gmail.com>
2020-09-25 01:29:44 +0600

    fixed the falg

--- flag/main.cpp
+++ flag/main.cpp
@@ -4,44 +4,48 @@
 #include<math.h>
 #define twicePi 6.28318
 
-void drawFilledelipse(GLfloat x, GLfloat y, GLfloat xcenter,GLfloat ycenter) {
-int i;
-int triangleAmount = 20; //# of triangles used to draw circle
+void drawFilledelipse(GLfloat x, GLfloat y, GLfloat xcenter,GLfloat ycenter)
+{
 
-//GLfloat radius = 0.8f; //radius
-//GLfloat twicePi = 2.0f * PI;
- glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0f,0.0f,0.0f);
-glBegin(GL_TRIANGLE_FAN);
-glVertex2f(x, y); // center of circle
-for (i = 0; i <= triangleAmount; i++) {
-    glVertex2f(
-        x + ((xcenter+1)* cos(i * twicePi / triangleAmount)),
-        y + ((ycenter-1)* sin(i * twicePi / triangleAmount))
-    );
-}
-glEnd();
- glFlush();
+    int triangleAmount = 100;
+    glBegin(GL_TRIANGLE_FAN);
+        glVertex2f(x, y);
+        for (int i = 0; i <= triangleAmount; i++)
+        {
+            glVertex2f(
+                x + ((xcenter)* cos(i * twicePi / triangleAmount)),
+                y + ((ycenter)* sin(i * twicePi / triangleAmount))
+            );
+        }
+    glEnd();
 }
 
 
-void init(){
-
-    glClearColor(0.0f,1.0f,0.0f,0.0f);
-    glOrtho(-6,6,-6,6,-6,6);
+void init()
+{
+    glClearColor(0.0f,0.0f,0.0f,0.0f);
+    glOrtho(-10,10,-10,10,-10,10);
 }
-static void display(void){
-    int x,y,i,radius=2;
-    x=4.5,y=3;
+static void display(void)
+{
+
     glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0f,1.0f,0.0f);
+    glBegin(GL_QUADS);
+        glVertex2d(5,3);
+        glVertex2d(5,-3);
+        glVertex2d(-5,-3);
+        glVertex2d(-5,3);
+    glEnd();
     glColor3f(1.0f,0.0f,0.0f);
-    drawFilledelipse(0,0,.2f,.1f);
+    drawFilledelipse(-1,0,2,2);
     glFlush();
 }
 
-int main(){
+int main()
+{
 
-    glutInitWindowSize(800,450);
+    glutInitWindowSize(700,700);
     glutInitWindowPosition(100,100);
     glutInitDisplayMode( GLUT_SINGLE|  GLUT_RGB);
     glutCreateWindow("BD flag");
