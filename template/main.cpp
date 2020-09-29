4c7091cad7c5db24d42ac0d3af9ce977860a93d0
shoccho <sfb.shoccho@gmail.com>
2020-09-30 00:36:35 +0600

    added some fonts in the  template

--- template/main.cpp
+++ template/main.cpp
@@ -4,33 +4,58 @@
 #include<math.h>
 
 
+//glut fonts
+const int font8x15=(int)GLUT_BITMAP_8_BY_13;
+const int font9x15=(int)GLUT_BITMAP_9_BY_15;
+const int fontTimes10=(int)GLUT_BITMAP_TIMES_ROMAN_10;
+const int fontTimes24=(int)GLUT_BITMAP_TIMES_ROMAN_24;
+const int fontHelvatica10=(int)GLUT_BITMAP_HELVETICA_10;
+const int fontHelvatica12=(int)GLUT_BITMAP_HELVETICA_12;
+const int fontHelvatica18=(int)GLUT_BITMAP_HELVETICA_18;
+
+
+
 static void idle(void)
 {
     glutPostRedisplay();
+}
+
+
+void renderBitmapString(float x, float y, void *font,const char *string)
+{
+    const char *c;
+    glRasterPos2f(x, y);
+    for (c=string; *c != '\0'; c++)
+    {
+        glutBitmapCharacter(font, *c);
+    }
 }
 static void key(unsigned char key, int x, int y)
 {
     glutPostRedisplay();
 }
-void init(){
+void init()
+{
 
     glClearColor(0.0f,0.0f,0.0f,0.0f);
     glOrtho(-10,10,-10,10,-10,10);
 }
 
-static void display(void){
+static void display(void)
+{
 
     glColor3f(1.0f,0.0f,0.0f);
     glBegin(GL_QUADS);
-        glVertex2d(5,2);
-        glVertex2d(5,-5);
-        glVertex2d(-5,-5);
-        glVertex2d(-5,2);
+    glVertex2d(5,2);
+    glVertex2d(5,-5);
+    glVertex2d(-5,-5);
+    glVertex2d(-5,2);
     glEnd();
     glFlush();
 }
 
-int main(){
+int main()
+{
 
     glutInitWindowSize(700,700);
     glutInitWindowPosition(100,100);
