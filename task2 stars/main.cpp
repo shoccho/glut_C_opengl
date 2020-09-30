757d7385c5560bd1d74cf1b83752332fc6aebe48
shoccho <sfb.shoccho@gmail.com>
2020-09-30 22:47:06 +0600

    cleaned up some code for submissions

--- task2 stars/main.cpp
+++ task2 stars/main.cpp
@@ -4,15 +4,6 @@
 #include<math.h>
 
 
-//glut fonts
-const int font8x15=(int)GLUT_BITMAP_8_BY_13;
-const int font9x15=(int)GLUT_BITMAP_9_BY_15;
-const int fontTimes10=(int)GLUT_BITMAP_TIMES_ROMAN_10;
-const int fontTimes24=(int)GLUT_BITMAP_TIMES_ROMAN_24;
-const int fontHelvatica10=(int)GLUT_BITMAP_HELVETICA_10;
-const int fontHelvatica12=(int)GLUT_BITMAP_HELVETICA_12;
-const int fontHelvatica18=(int)GLUT_BITMAP_HELVETICA_18;
-
 int orthox=10;
 int orthoy=10;
 int orthoz=10;
@@ -22,15 +13,6 @@ static void idle(void)
     glutPostRedisplay();
 }
 
-void renderBitmapString(float x, float y, void *font,const char *string)
-{
-    const char *c;
-    glRasterPos2f(x, y);
-    for (c=string; *c != '\0'; c++)
-    {
-        glutBitmapCharacter(font, *c);
-    }
-}
 static void key(unsigned char key, int x, int y)
 {
     glutPostRedisplay();
