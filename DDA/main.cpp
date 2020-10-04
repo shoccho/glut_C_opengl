0e6a1b08658bfc1a2b247fa1123d5fcf9c18487f
shoccho <sfb.shoccho@gmail.com>
2020-10-04 21:08:28 +0600

    finished DDA

--- DDA/main.cpp
+++ DDA/main.cpp
@@ -2,7 +2,7 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 #include<math.h>
-
+#include<bits/stdc++.h>
 
 //glut fonts
 const int font8x15=(int)GLUT_BITMAP_8_BY_13;
@@ -16,6 +16,9 @@ const int fontHelvatica18=(int)GLUT_BITMAP_HELVETICA_18;
 int orthox=700;
 int orthoy=700;
 int orthoz=700;
+
+std::vector<int>xpoints;
+std::vector<int>ypoints;
 
 static void idle(void)
 {
@@ -40,8 +43,9 @@ void init()
     glClearColor(0.0f,0.0f,0.0f,0.0f);
     glOrtho(-orthox,orthox,-orthoy,orthoy,-orthoz,orthoz);
 }
-void showgrid(){
-  void showgrid(){
+
+void showgrid()
+{
     glBegin(GL_LINES);
     glColor3f(.5f,.5f,.5f);
     for (int i=-orthoy; i<=orthoy; i++)
@@ -63,18 +67,72 @@ void showgrid(){
     glVertex2d(0,-orthoy);
     glEnd();
 }
-}
+
 
 static void display(void)
 {
-
-    showgrid();
+    glClear(GL_COLOR_BUFFER_BIT );
+    glColor3f(1.0f,1.0f,1.0f);
+    glBegin(GL_POINTS);
+    for(int i=0; i<xpoints.size(); i++)
+    {
+        glVertex2d(xpoints[i],ypoints[i]);
+    }
+    //showgrid();
+    glEnd();
     glFlush();
+}
+int mkp(float x)
+{
+    if(int(x*10)%10 <5)return floor(x);
+    return ceil(x);
+}
+void dda(double m, float sx,float sy, float ex,float ey)
+{
+    float x, y;
+    float dx,dy;
+    x= sx;
+    y= sy;
+
+    if( m <= 1)
+    {
+        dx = 1;
+        dy = m;
+
+    }
+    else if( m > 1)
+    {
+        dy = 1;
+        dx = 1/m;
+    }
+    if (sx > ex)
+    {
+        dx = -1*dx;
+        dy = -1*dy;
+    }
+    //print("m={0:.2f} Dx = {1:.2f} Dy = {2:.2f} ".format(m,dx,dy))
+    //print("x\ty\t\tx plot\ty plot\t\t(x, y)")
+
+    while(1)
+    {
+        int px=mkp(x);
+        int py=mkp(y);
+
+        xpoints.push_back(px);
+        ypoints.push_back(py);
+       // print("{0}\t{1:.2f}\t\t{2}\t{3}\t\t({2}, {3})".format(x,y,px,py));
+        x += dx;
+        y += dy;
+        if (px==ex && py==ey)break;
+    }
+    return;
 }
 
 int main()
 {
-
+    float sx=0,sy=0,ex=100,ey=100;
+    double m =((ey-sy)/(ex-sx));
+    dda(m,sx,sy,ex,ey);
     glutInitWindowSize(700,700);
     glutInitWindowPosition(300,100);
     glutInitDisplayMode( GLUT_SINGLE|  GLUT_RGB);
