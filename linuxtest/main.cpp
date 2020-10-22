0cd48539c22cbec56b321a8e00c2c7f78a4f1e9d
shoccho <sfb.shoccho@gmail.com>
2020-10-22 23:06:39 +0600

    did some 3d

--- linuxtest/main.cpp
+++ linuxtest/main.cpp
@@ -1,26 +1,97 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <GL/glut.h>
-
-//Drawing funciton
+float x = 0;
+float dx = 1;
+void init()
+{
+    glClearColor(0, 0, 0, 1);
+    glEnable(GL_DEPTH_TEST);
+}
 void draw(void)
 {
-    //Background color
-    glClearColor(0, 1, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-    //Draw order
-    glFlush();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    glColor3d(1, 0, 0);
+    glPushMatrix();
+    glTranslatef(0, 0.0f, -8.0f);
+    glRotated(x, 1, 1, 1);
+    //glTranslatef(0, 0.0f, -8.0f);
+    glBegin(GL_QUADS);
+    //zf
+    glColor3d(1, 0, 0);
+    glVertex3d(1, 1, 1);
+    glVertex3d(1, -1, 1);
+    glVertex3d(-1, -1, 1);
+    glVertex3d(-1, 1, 1);
+
+    glColor3d(0, 1, 0);
+    glVertex3d(1, 1, -1);
+    glVertex3d(1, -1, -1);
+    glVertex3d(-1, -1, -1);
+    glVertex3d(-1, 1, -1);
+
+    //xf
+    glColor3d(0, 0, 1);
+    glVertex3d(1, 1, 1);
+    glVertex3d(1, 1, -1);
+    glVertex3d(1, -1, -1);
+    glVertex3d(1, -1, 1);
+
+    glColor3d(1, 0, 1);
+    glVertex3d(-1, 1, 1);
+    glVertex3d(-1, 1, -1);
+    glVertex3d(-1, -1, -1);
+    glVertex3d(-1, -1, 1);
+
+    //yf
+    glColor3d(1, 1, 0);
+    glVertex3d(1, 1, 1);
+    glVertex3d(1, 1, -1);
+    glVertex3d(-1, 1, -1);
+    glVertex3d(-1, 1, 1);
+
+    glColor3d(1, 1, 1);
+    glVertex3d(1, -1, 1);
+    glVertex3d(1, -1, -1);
+    glVertex3d(-1, -1, -1);
+    glVertex3d(-1, -1, 1);
+
+    glEnd();
+    glPopMatrix();
+    //glFlush();
+    glutSwapBuffers();
+}
+void timer(int)
+{
+    glutPostRedisplay();
+    glutTimerFunc(1000 / 60, timer, 0);
+    x += dx;
+    if (x > 360)
+        x = 0;
+}
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //gluOrtho2D(-10, 10, -10, 10);
+    gluPerspective(60.0, 1, 2, 50);
+    glMatrixMode(GL_MODELVIEW);
 }
 
-//Main program
 int main(int argc, char **argv)
 {
+
     glutInit(&argc, argv);
-    //Simple buffer
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowPosition(50, 25);
-    glutInitWindowSize(500, 250);
-    glutCreateWindow("Green window");
-    //Call to the drawing function
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("linux test");
     glutDisplayFunc(draw);
+    glutReshapeFunc(reshape);
+    glutTimerFunc(0, timer, 0);
+    init();
     glutMainLoop();
     return 0;
 }
--- terrain/main.cpp
+++ terrain/main.cpp
@@ -1,32 +1,30 @@
-#include<windows.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <GL/glut.h>
 #include <stdlib.h>
-#include<math.h>
-
+#include <math.h>
 
 //glut fonts
-const int font8x15=(int)GLUT_BITMAP_8_BY_13;
-const int font9x15=(int)GLUT_BITMAP_9_BY_15;
-const int fontTimes10=(int)GLUT_BITMAP_TIMES_ROMAN_10;
-const int fontTimes24=(int)GLUT_BITMAP_TIMES_ROMAN_24;
-const int fontHelvatica10=(int)GLUT_BITMAP_HELVETICA_10;
-const int fontHelvatica12=(int)GLUT_BITMAP_HELVETICA_12;
-const int fontHelvatica18=(int)GLUT_BITMAP_HELVETICA_18;
+// const int font8x15 = GLUT_BITMAP_8_BY_13;
+// const int font9x15 = GLUT_BITMAP_9_BY_15;
+// const int fontTimes10 = GLUT_BITMAP_TIMES_ROMAN_10;
+// const int fontTimes24 = GLUT_BITMAP_TIMES_ROMAN_24;
+// const int fontHelvatica10 = GLUT_BITMAP_HELVETICA_10;
+// const int fontHelvatica12 = GLUT_BITMAP_HELVETICA_12;
+// const int fontHelvatica18 = GLUT_BITMAP_HELVETICA_18;
 
-int orthox=10;
-int orthoy=10;
-int orthoz=10;
+//global stuff
+int width = 600, height = 600;
 
-static void idle(void)
-{
-    glutPostRedisplay();
-}
+//stuffs
+int cols, rows;
+int scl = 20;
 
-void renderBitmapString(float x, float y, void *font,const char *string)
+void renderBitmapString(float x, float y, void *font, const char *string)
 {
     const char *c;
     glRasterPos2f(x, y);
-    for (c=string; *c != '\0'; c++)
+    for (c = string; *c != '\0'; c++)
     {
         glutBitmapCharacter(font, *c);
     }
@@ -37,57 +35,72 @@ static void key(unsigned char key, int x, int y)
 }
 void init()
 {
-    glClearColor(0.0f,0.0f,0.0f,0.0f);
-    glOrtho(-orthox,orthox,-orthoy,orthoy,-orthoz,orthoz);
+    glClearColor(0, 0, 0, 1);
+    glEnable(GL_DEPTH_TEST);
+    cols = width / scl;
+    rows = height / scl;
 }
-void showgrid(){
-    glBegin(GL_LINES);
-    glColor3f(.5f,.5f,.5f);
-    for (int i=-orthoy; i<=orthoy; i++)
-    {
-        glVertex3d(10,i,0);
-        glVertex3d(-10,i,0);
-
-    }
-    for (int i=-orthox; i<=orthox; i++)
-    {
-        glVertex3d(i,10,0);
-        glVertex3d(i,-10,0);
-
-    }
-    glColor3f(0.0f,0.0f,1.0f);
-    glVertex2d(-10,0);
-    glVertex2d(10,0);
-    glVertex2d(0,10);
-    glVertex2d(0,-10);
-    glEnd();
-}
-
-static void display(void)
+void draw(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    glColor3d(1, 0, 0);
 
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPushMatrix();
+    glTranslatef(0, 0.0f, -8.0f);
+    //glRotatef(30, 1, 0, 0);
+    // glPushMatrix();
+    // for (int x = 0; x < cols; x++)
+    // {
+    //     glBegin(GL_TRIANGLE_STRIP);
+    //     for (int y = 0; y < rows; y++)
+    //     {
+    //         glVertex3d(x * scl, y * scl, 0);
+    //         //glVertex3d((x + 1) * scl, y * scl, 0);
+    //         glVertex3d((x)*scl, (y + 1) * scl, 0);
+    //     }
+    //     glEnd();
+    // }
 
-    glRotated(45,0,0,0);
-showgrid();
+    glBegin(GL_LINES);
 
+    glVertex3d(0, 0, 0);
+    glColor3f(0, 1, 0);
+    glVertex3d(width, height, 0);
+    glEnd();
+    glPopMatrix();
     //glFlush();
-       glutSwapBuffers();
+    glutSwapBuffers();
 }
-
-int main()
+void timer(int)
+{
+    glutPostRedisplay();
+    glutTimerFunc(1000 / 60, timer, 0);
+    //do update here
+}
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //gluOrtho2D(-10, 10, -10, 10);
+    gluPerspective(60.0, 1, 2, 50);
+    glMatrixMode(GL_MODELVIEW);
+}
+int main(int argc, char **argv)
 {
 
-    glutInitWindowSize(700,700);
-    glutInitWindowPosition(300,100);
-    glutInitDisplayMode( GLUT_DOUBLE|  GLUT_RGB);
-    glutCreateWindow("Terrain");
-
-    init();
-    glutDisplayFunc(display);
-    glutIdleFunc(idle);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowPosition(50, 25);
+    glutInitWindowSize(width, height);
+    glutCreateWindow("template");
+    glutDisplayFunc(draw);
+    glutReshapeFunc(reshape);
+    glutTimerFunc(0, timer, 0);
     glutKeyboardFunc(key);
+    init();
     glutMainLoop();
-
-    return EXIT_SUCCESS;
+    return 0;
 }
